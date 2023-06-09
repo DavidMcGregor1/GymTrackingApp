18cf3553cfd6e4b211dc8e5d5ac634f3f66b0a16
David McGregor <david@plisky.net>
2023-06-09 10:48:00 +0100

    added getAllExercises endpoint

--- src/main/java/com/example/GymTrackingApp/MainController.java
+++ src/main/java/com/example/GymTrackingApp/MainController.java
@@ -1,5 +1,6 @@
 package com.example.GymTrackingApp;
 
+import org.jboss.jandex.Main;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +12,41 @@ import java.util.*;
 @Controller
 public class MainController {
 
+    public MainController(ExercisesRepository e) {
+        repositoryExercises = e;
+    }
+
+    private ExercisesRepository repositoryExercises;
+
     @GetMapping("/hello")
     public String sayHello() {
         System.out.println("arrived at hello page");
         return "index";
+    }
+
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
+    @GetMapping(path = "/getAllExercises", consumes = "application/json", produces = "application/json")
+    public String getAllExercises() {
+        System.out.println("Hit getAllExercises endpoint");
+
+        List<Exercises> allDbExercises = repositoryExercises.findAll();
+
+        String result = "All Exercises ---> ";
+
+        for (int i = 0; i < allDbExercises.stream().count(); i++) {
+            Exercises a = allDbExercises.get(i);
+
+            if (a != null) {
+                result = result + a.getExerciseName() + ", ";
+            }
+        }
+
+        return result;
+
+
+
     }
 
 }

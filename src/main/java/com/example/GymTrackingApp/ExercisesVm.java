fd2432060d805c8eff210b82751edc931e21c092
David McGregor <david@plisky.net>
2023-06-09 10:24:21 +0100

    added Exercises view model

---
+++ src/main/java/com/example/GymTrackingApp/ExercisesVm.java
@@ -0,0 +1,17 @@
+package com.example.GymTrackingApp;
+
+public class ExercisesVm {
+    public int id;
+    public String exerciseName;
+
+
+    public ExercisesVm(int aId, String aExerciseName) {
+        id = aId;
+        exerciseName = aExerciseName;
+    }
+
+    public ExercisesVm() {
+
+    }
+
+}

54898965460cf68111aad9def393cc4bbb899688
David McGregor <david@plisky.net>
2023-06-09 14:07:45 +0100

    added categoriesVm

---
+++ src/main/java/com/example/GymTrackingApp/CategoriesVm.java
@@ -0,0 +1,16 @@
+package com.example.GymTrackingApp;
+
+public class CategoriesVm {
+    public int id;
+    public String category;
+
+    CategoriesVm(int aId, String aCategory) {
+        id = aId;
+        category = aCategory;
+    }
+
+    public CategoriesVm() {
+        
+    }
+
+}
